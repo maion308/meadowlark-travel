0c13938d6b312a9b15a24f2bc2494e325134e073
maion308 <michael.ion.mi@outlook.com>
2019-12-11 23:53:00 -0500

    created views

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const app = express()
 const port = process.env.PORT || 8000
 
 app.get('/', (req, res) => {
-    res.status(200)
-    res.send('Meadowlark Travel')
+    res.render('home')
 })
 
 app.get('/about', (req, res) => {
