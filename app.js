c6c02769149e6d4dc15cdc92e20861f8e2e8a6da
maion308 <michael.ion.mi@outlook.com>
2019-12-11 23:40:35 -0500

    set up middleware

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 8000
 
-
 app.get('/', (req, res) => {
     res.status(200)
     res.send('Meadowlark Travel')
