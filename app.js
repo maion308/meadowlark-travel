8ce7718cc7042cfe5413382fa0e1a7e806c3b7a3
maion308 <michael.ion.mi@outlook.com>
2019-12-11 23:35:29 -0500

    set up express server, serving up home and about page

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 8000
+
+
+app.get('/', (req, res) => {
+    res.status(200)
+    res.send('Meadowlark Travel')
+})
+
+app.get('/about', (req, res) => {
+    res.status(200)
+    res.send('About Meadowlark Travel')
+})
+
+
+// Middlware
+app.use((req,res,next) => {
+    res.status(404)
+    res.send('404 - Not Found')
+})
+
+app.use((err, req,res,next) => {
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
